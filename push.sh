4338388e1e92cd96bba27dff95d4fa88d6f860e4
hemanth22 <hemanth22hemu@gmail.com>
2018-04-21 17:20:40 +0530

    Correct commands in travis and push command

--- push.sh
+++ push.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 setup_git() {
-  git config --global user.email "hemanth22hemu@gmail.com"
-  git config --global user.name "hemanth22"
+git config --global user.email "hemanth22hemu@gmail.com"
+git config --global user.name "hemanth22"
 }
 
 commit_website_files() {
-  git add .
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git add .
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin master
+git remote add origin https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+git push --quiet --set-upstream origin master
 }
 
 setup_git
